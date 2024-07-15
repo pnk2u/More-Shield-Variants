825d24325336cb9bd7029b33f8ecd87ffb261870
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-07-15 13:25:46 +0200

    Add missing files and config for all wood types

--- src/client/java/de/pnku/lolmsv/config/MoreShieldVariantsConfig.java
+++ src/client/java/de/pnku/lolmsv/config/MoreShieldVariantsConfig.java
@@ -6,11 +6,31 @@ import java.util.List;
 public class MoreShieldVariantsConfig {
 
     private boolean acaciaUseCustom;
+    private boolean bambooUseCustom;
+    private boolean birchUseCustom;
+    private boolean cherryUseCustom;
+    private boolean crimsonUseCustom;
+    private boolean darkOakUseCustom;
+    private boolean jungleUseCustom;
+    private boolean mangroveUseCustom;
+    private boolean oakUseCustom;
+    private boolean spruceUseCustom;
+    private boolean warpedUseCustom;
     private static MoreShieldVariantsConfig INSTANCE;
     public static List<String> textureConfigList = new ArrayList<>();
 
     public MoreShieldVariantsConfig() {
         acaciaUseCustom = true;
+        bambooUseCustom = true;
+        birchUseCustom = true;
+        cherryUseCustom = true;
+        crimsonUseCustom = true;
+        darkOakUseCustom = true;
+        jungleUseCustom = true;
+        mangroveUseCustom = true;
+        oakUseCustom = true;
+        spruceUseCustom = true;
+        warpedUseCustom = true;
     }
 
     public static MoreShieldVariantsConfig getInstance() {
@@ -29,7 +49,99 @@ public class MoreShieldVariantsConfig {
 
     public boolean isAcaciaUseCustom() { return acaciaUseCustom; }
 
+    public void setBambooUseCustom(boolean bambooUseCustom) {
+        this.bambooUseCustom = bambooUseCustom;
+        if (isBambooUseCustom()) textureConfigList.add("bamboo");
+        else textureConfigList.remove("bamboo");
+    }
+
+    public boolean isBambooUseCustom() { return bambooUseCustom; }
+
+    public void setBirchUseCustom(boolean birchUseCustom) {
+        this.birchUseCustom = birchUseCustom;
+        if (isBirchUseCustom()) textureConfigList.add("birch");
+        else textureConfigList.remove("birch");
+    }
+
+    public boolean isBirchUseCustom() { return birchUseCustom; }
+
+    public void setCherryUseCustom(boolean cherryUseCustom) {
+        this.cherryUseCustom = cherryUseCustom;
+        if (isCherryUseCustom()) textureConfigList.add("cherry");
+        else textureConfigList.remove("cherry");
+    }
+
+    public boolean isCherryUseCustom() { return cherryUseCustom; }
+
+    public void setCrimsonUseCustom(boolean crimsonUseCustom) {
+        this.crimsonUseCustom = crimsonUseCustom;
+        if (isCrimsonUseCustom()) textureConfigList.add("crimson");
+        else textureConfigList.remove("crimson");
+    }
+
+    public boolean isCrimsonUseCustom() { return crimsonUseCustom; }
+
+    public void setDarkOakUseCustom(boolean darkOakUseCustom) {
+        this.darkOakUseCustom = darkOakUseCustom;
+        if (isDarkOakUseCustom()) textureConfigList.add("dark_oak");
+        else textureConfigList.remove("dark_oak");
+    }
+
+    public boolean isDarkOakUseCustom() { return darkOakUseCustom; }
+
+    public void setJungleUseCustom(boolean jungleUseCustom) {
+        this.jungleUseCustom = jungleUseCustom;
+        if (isJungleUseCustom()) textureConfigList.add("jungle");
+        else textureConfigList.remove("jungle");
+    }
+
+    public boolean isJungleUseCustom() { return jungleUseCustom; }
+
+    public void setMangroveUseCustom(boolean mangroveUseCustom) {
+        this.mangroveUseCustom = mangroveUseCustom;
+        if (isMangroveUseCustom()) textureConfigList.add("mangrove");
+        else textureConfigList.remove("mangrove");
+    }
+
+    public boolean isMangroveUseCustom() { return mangroveUseCustom; }
+
+    public void setOakUseCustom(boolean oakUseCustom) {
+        this.oakUseCustom = oakUseCustom;
+        if (isOakUseCustom()) textureConfigList.add("oak");
+        else textureConfigList.remove("oak");
+    }
+
+    public boolean isOakUseCustom() { return oakUseCustom; }
+
+    public void setSpruceUseCustom(boolean spruceUseCustom) {
+        this.spruceUseCustom = spruceUseCustom;
+        if (isSpruceUseCustom()) textureConfigList.add("spruce");
+        else textureConfigList.remove("spruce");
+    }
+
+    public boolean isSpruceUseCustom() { return spruceUseCustom; }
+
+    public void setWarpedUseCustom(boolean warpedUseCustom) {
+        this.warpedUseCustom = warpedUseCustom;
+        if (isWarpedUseCustom()) textureConfigList.add("warped");
+        else textureConfigList.remove("warped");
+    }
+
+    public boolean isWarpedUseCustom() { return warpedUseCustom; }
+
+
+
     public void updateConfigs(MoreShieldVariantsConfig config) {
         acaciaUseCustom = config.isAcaciaUseCustom();
+        bambooUseCustom = config.isBambooUseCustom();
+        birchUseCustom = config.isBirchUseCustom();
+        cherryUseCustom = config.isCherryUseCustom();
+        crimsonUseCustom = config.isCrimsonUseCustom();
+        darkOakUseCustom = config.isDarkOakUseCustom();
+        jungleUseCustom = config.isJungleUseCustom();
+        mangroveUseCustom = config.isMangroveUseCustom();
+        oakUseCustom = config.isOakUseCustom();
+        spruceUseCustom = config.isSpruceUseCustom();
+        warpedUseCustom = config.isWarpedUseCustom();
     }
 }
--- src/client/java/de/pnku/lolmsv/config/MoreShieldVariantsConfigScreen.java
+++ src/client/java/de/pnku/lolmsv/config/MoreShieldVariantsConfigScreen.java
@@ -23,6 +23,76 @@ public class MoreShieldVariantsConfigScreen implements ModMenuApi {
                     MoreShieldVariantsConfig.getInstance().setAcaciaUseCustom(acaciaUseCustomBoolean);})
                 .setTooltip(Component.translatable("config.shieldTexture_option.moreshieldvariants.acaciaUseCustom.tooltip"))
                 .build());
+        shieldTexture.addEntry(configBuilder.entryBuilder()
+                .startBooleanToggle(Component.translatable("config.shieldTexture_option.moreshieldvariants.bambooUseCustom"), MoreShieldVariantsConfig.getInstance().isBambooUseCustom())
+                .setDefaultValue(true)
+                .setSaveConsumer(bambooUseCustomBoolean -> {
+                    MoreShieldVariantsConfig.getInstance().setBambooUseCustom(bambooUseCustomBoolean);})
+                .setTooltip(Component.translatable("config.shieldTexture_option.moreshieldvariants.bambooUseCustom.tooltip"))
+                .build());
+        shieldTexture.addEntry(configBuilder.entryBuilder()
+                .startBooleanToggle(Component.translatable("config.shieldTexture_option.moreshieldvariants.birchUseCustom"), MoreShieldVariantsConfig.getInstance().isBirchUseCustom())
+                .setDefaultValue(true)
+                .setSaveConsumer(birchUseCustomBoolean -> {
+                    MoreShieldVariantsConfig.getInstance().setBirchUseCustom(birchUseCustomBoolean);})
+                .setTooltip(Component.translatable("config.shieldTexture_option.moreshieldvariants.birchUseCustom.tooltip"))
+                .build());
+        shieldTexture.addEntry(configBuilder.entryBuilder()
+                .startBooleanToggle(Component.translatable("config.shieldTexture_option.moreshieldvariants.cherryUseCustom"), MoreShieldVariantsConfig.getInstance().isCherryUseCustom())
+                .setDefaultValue(true)
+                .setSaveConsumer(cherryUseCustomBoolean -> {
+                    MoreShieldVariantsConfig.getInstance().setCherryUseCustom(cherryUseCustomBoolean);})
+                .setTooltip(Component.translatable("config.shieldTexture_option.moreshieldvariants.cherryUseCustom.tooltip"))
+                .build());
+        shieldTexture.addEntry(configBuilder.entryBuilder()
+                .startBooleanToggle(Component.translatable("config.shieldTexture_option.moreshieldvariants.crimsonUseCustom"), MoreShieldVariantsConfig.getInstance().isCrimsonUseCustom())
+                .setDefaultValue(true)
+                .setSaveConsumer(crimsonUseCustomBoolean -> {
+                    MoreShieldVariantsConfig.getInstance().setCrimsonUseCustom(crimsonUseCustomBoolean);})
+                .setTooltip(Component.translatable("config.shieldTexture_option.moreshieldvariants.crimsonUseCustom.tooltip"))
+                .build());
+        shieldTexture.addEntry(configBuilder.entryBuilder()
+                .startBooleanToggle(Component.translatable("config.shieldTexture_option.moreshieldvariants.darkOakUseCustom"), MoreShieldVariantsConfig.getInstance().isDarkOakUseCustom())
+                .setDefaultValue(true)
+                .setSaveConsumer(darkOakUseCustomBoolean -> {
+                    MoreShieldVariantsConfig.getInstance().setDarkOakUseCustom(darkOakUseCustomBoolean);})
+                .setTooltip(Component.translatable("config.shieldTexture_option.moreshieldvariants.darkOakUseCustom.tooltip"))
+                .build());
+        shieldTexture.addEntry(configBuilder.entryBuilder()
+                .startBooleanToggle(Component.translatable("config.shieldTexture_option.moreshieldvariants.jungleUseCustom"), MoreShieldVariantsConfig.getInstance().isJungleUseCustom())
+                .setDefaultValue(true)
+                .setSaveConsumer(jungleUseCustomBoolean -> {
+                    MoreShieldVariantsConfig.getInstance().setJungleUseCustom(jungleUseCustomBoolean);})
+                .setTooltip(Component.translatable("config.shieldTexture_option.moreshieldvariants.jungleUseCustom.tooltip"))
+                .build());
+        shieldTexture.addEntry(configBuilder.entryBuilder()
+                .startBooleanToggle(Component.translatable("config.shieldTexture_option.moreshieldvariants.mangroveUseCustom"), MoreShieldVariantsConfig.getInstance().isMangroveUseCustom())
+                .setDefaultValue(true)
+                .setSaveConsumer(mangroveUseCustomBoolean -> {
+                    MoreShieldVariantsConfig.getInstance().setMangroveUseCustom(mangroveUseCustomBoolean);})
+                .setTooltip(Component.translatable("config.shieldTexture_option.moreshieldvariants.mangroveUseCustom.tooltip"))
+                .build());
+        shieldTexture.addEntry(configBuilder.entryBuilder()
+                .startBooleanToggle(Component.translatable("config.shieldTexture_option.moreshieldvariants.oakUseCustom"), MoreShieldVariantsConfig.getInstance().isOakUseCustom())
+                .setDefaultValue(true)
+                .setSaveConsumer(oakUseCustomBoolean -> {
+                    MoreShieldVariantsConfig.getInstance().setOakUseCustom(oakUseCustomBoolean);})
+                .setTooltip(Component.translatable("config.shieldTexture_option.moreshieldvariants.oakUseCustom.tooltip"))
+                .build());
+        shieldTexture.addEntry(configBuilder.entryBuilder()
+                .startBooleanToggle(Component.translatable("config.shieldTexture_option.moreshieldvariants.spruceUseCustom"), MoreShieldVariantsConfig.getInstance().isSpruceUseCustom())
+                .setDefaultValue(true)
+                .setSaveConsumer(spruceUseCustomBoolean -> {
+                    MoreShieldVariantsConfig.getInstance().setSpruceUseCustom(spruceUseCustomBoolean);})
+                .setTooltip(Component.translatable("config.shieldTexture_option.moreshieldvariants.spruceUseCustom.tooltip"))
+                .build());
+        shieldTexture.addEntry(configBuilder.entryBuilder()
+                .startBooleanToggle(Component.translatable("config.shieldTexture_option.moreshieldvariants.warpedUseCustom"), MoreShieldVariantsConfig.getInstance().isWarpedUseCustom())
+                .setDefaultValue(true)
+                .setSaveConsumer(warpedUseCustomBoolean -> {
+                    MoreShieldVariantsConfig.getInstance().setWarpedUseCustom(warpedUseCustomBoolean);})
+                .setTooltip(Component.translatable("config.shieldTexture_option.moreshieldvariants.warpedUseCustom.tooltip"))
+                .build());
         return configBuilder;
 
     }
