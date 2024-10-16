68537ca14d6482dbfb9650ca76e9bb6f2191b559
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-10-16 20:46:23 +0200

    cm tabs registration to client

--- src/client/java/de/pnku/lolmsv/MoreShieldVariantsClient.java
+++ src/client/java/de/pnku/lolmsv/MoreShieldVariantsClient.java
@@ -2,6 +2,7 @@ package de.pnku.lolmsv;
 
 import de.pnku.lolmsv.config.MoreShieldVariantsConfig;
 import de.pnku.lolmsv.config.MoreShieldVariantsConfigJsonHelper;
+import de.pnku.lolmsv.item.MoreShieldVariantItemGroups;
 import de.pnku.lolmsv.item.MoreShieldVariantModelPredicateProvider;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
@@ -17,7 +18,7 @@ public class MoreShieldVariantsClient implements ClientModInitializer {
         MoreShieldVariantModelPredicateProvider.registerMoreShieldVariantItemModelPredicates();
         MoreShieldVariantsConfigJsonHelper.init();
         MoreShieldVariantsConfig.initialReadConfig();
-        
+        MoreShieldVariantItemGroups.registerMoreShieldVariantItemGroups();
     }
     
 }
