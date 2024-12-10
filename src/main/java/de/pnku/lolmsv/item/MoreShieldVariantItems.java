b4db32e959fb6fefb42ecd9839723f56fcbc7a7d
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-12-10 05:06:10 +0100

    revert commit b2d38c8 from merge
    Co-Authored-By: StarmanMine142(or just Starman) <135715391+starmanmine142@users.noreply.github.com>

--- src/main/java/de/pnku/lolmsv/item/MoreShieldVariantItems.java
+++ src/main/java/de/pnku/lolmsv/item/MoreShieldVariantItems.java
@@ -1,36 +1,34 @@
 package de.pnku.lolmsv.item;
 
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.*;
 import net.minecraft.core.Registry;
 import de.pnku.lolmsv.MoreShieldVariants;
-import net.minecraft.world.level.block.entity.BannerPatternLayers;
 
 
 public class MoreShieldVariantItems {
 
     // Shields
-    public static final Item ACACIA_SHIELD = new MoreShieldVariantItem("acacia", new Item.Properties().durability(336).component(DataComponents.BANNER_PATTERNS, BannerPatternLayers.EMPTY));
+    public static final Item ACACIA_SHIELD = new MoreShieldVariantItem("acacia", new Item.Properties().durability(336));
     
-    public static final Item BAMBOO_SHIELD = new MoreShieldVariantItem("bamboo", new Item.Properties().durability(336).component(DataComponents.BANNER_PATTERNS, BannerPatternLayers.EMPTY));
+    public static final Item BAMBOO_SHIELD = new MoreShieldVariantItem("bamboo", new Item.Properties().durability(336));
 
-    public static final Item BIRCH_SHIELD = new MoreShieldVariantItem("birch", new Item.Properties().durability(336).component(DataComponents.BANNER_PATTERNS, BannerPatternLayers.EMPTY));
+    public static final Item BIRCH_SHIELD = new MoreShieldVariantItem("birch", new Item.Properties().durability(336));
     
-    public static final Item CHERRY_SHIELD = new MoreShieldVariantItem("cherry", new Item.Properties().durability(336).component(DataComponents.BANNER_PATTERNS, BannerPatternLayers.EMPTY));
+    public static final Item CHERRY_SHIELD = new MoreShieldVariantItem("cherry", new Item.Properties().durability(336));
 
-    public static final Item CRIMSON_SHIELD = new MoreShieldVariantItem("crimson", new Item.Properties().durability(337).fireResistant().component(DataComponents.BANNER_PATTERNS, BannerPatternLayers.EMPTY));
+    public static final Item CRIMSON_SHIELD = new MoreShieldVariantItem("crimson", new Item.Properties().durability(337).fireResistant());
 
-    public static final Item DARK_OAK_SHIELD = new MoreShieldVariantItem("dark_oak", new Item.Properties().durability(336).component(DataComponents.BANNER_PATTERNS, BannerPatternLayers.EMPTY));
+    public static final Item DARK_OAK_SHIELD = new MoreShieldVariantItem("dark_oak", new Item.Properties().durability(336));
 
-    public static final Item JUNGLE_SHIELD = new MoreShieldVariantItem("jungle", new Item.Properties().durability(336).component(DataComponents.BANNER_PATTERNS, BannerPatternLayers.EMPTY));
+    public static final Item JUNGLE_SHIELD = new MoreShieldVariantItem("jungle", new Item.Properties().durability(336));
     
-    public static final Item MANGROVE_SHIELD = new MoreShieldVariantItem("mangrove", new Item.Properties().durability(336).component(DataComponents.BANNER_PATTERNS, BannerPatternLayers.EMPTY));
+    public static final Item MANGROVE_SHIELD = new MoreShieldVariantItem("mangrove", new Item.Properties().durability(336));
     
-    public static final Item OAK_SHIELD = new MoreShieldVariantItem("oak", new Item.Properties().durability(336).component(DataComponents.BANNER_PATTERNS, BannerPatternLayers.EMPTY));
+    public static final Item OAK_SHIELD = new MoreShieldVariantItem("oak", new Item.Properties().durability(336));
     
-    public static final Item WARPED_SHIELD = new MoreShieldVariantItem("warped", new Item.Properties().durability(337).fireResistant().component(DataComponents.BANNER_PATTERNS, BannerPatternLayers.EMPTY));
+    public static final Item WARPED_SHIELD = new MoreShieldVariantItem("warped", new Item.Properties().durability(337).fireResistant());
 
     public static void registerShieldItems() {
         registerShieldItem(ACACIA_SHIELD);
